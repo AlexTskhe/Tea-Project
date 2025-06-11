2b2b92f38b24a35412f023f49fd7c4d5db7bcf8d
mrkqstn <myiskra@icloud.com>
2025-06-11 14:30:17 +0300

    fix server start

--- server/src/routes/indexRouter.js
+++ server/src/routes/indexRouter.js
@@ -1,12 +1,12 @@
 const router = require('express').Router(); //* получаем экземпляр роутинга из библиотеки
 const teaRouter = require('./teaRouter'); //* подтягиваем набор роутинга по определенному пути
 const formatResponse = require('../utils/formatResponse'); //* подтягиваем утилиту для унификации ответа по 404
-const authRouter = require('./authRouter');
-const commentRouter = require('./commentRouter');
+// const authRouter = require('./authRouter');
+// const commentRouter = require('./commentRouter');
 
 router.use('/teas', teaRouter); //* по пути на posts отрабатывает набор из postRouter
-router.use('/auth', authRouter);
-router.use('/comments', commentRouter);
+// router.use('/auth', authRouter);
+// router.use('/comments', commentRouter);
   
 //! Обработка всех запросов на несуществующие маршруты (меняем стандартный ответ от express)
 router.use((req, res) => {
--- server/src/routes/teaRouter.js
+++ server/src/routes/teaRouter.js
@@ -1,18 +1,24 @@
-const express = require("express");
-const TeaController = require("../controllers/TeaController");
-const validateId = require("../middlewares/validateId");
-const {
-  verifyRefreshToken,
-  verifyAccessToken,
-} = require("../middlewares/verifyTokens");
+const express = require('express');
+const TeaController = require('../controllers/TeaController');
+const validateId = require('../middlewares/validateId');
+// const {
+//   verifyRefreshToken,
+//   verifyAccessToken,
+// } = require("../middlewares/verifyTokens");
 
 // экземляр маршрута
 const teaRouter = express.Router();
 
-teaRouter.get("/", TeaController.getTeas);
-teaRouter.post("/", verifyAccessToken, TeaController.createTea);
-teaRouter.get("/:id", validateId, TeaController.getTeaById);
-teaRouter.put("/:id", verifyAccessToken, validateId, TeaController.updateTea);
-teaRouter.delete("/:id", verifyAccessToken, validateId, TeaController.deleteTea);
+// teaRouter.get("/", TeaController.getTeas);
+// teaRouter.post("/", verifyAccessToken, TeaController.createTea);
+// teaRouter.get("/:id", validateId, TeaController.getTeaById);
+// teaRouter.put("/:id", verifyAccessToken, validateId, TeaController.updateTea);
+// teaRouter.delete("/:id", verifyAccessToken, validateId, TeaController.deleteTea);
+
+teaRouter.get('/', TeaController.getTeas);
+teaRouter.post('/', TeaController.createTea);
+teaRouter.get('/:id', validateId, TeaController.getTeaById);
+teaRouter.put('/:id', validateId, TeaController.updateTea);
+teaRouter.delete('/:id', validateId, TeaController.deleteTea);
 
 module.exports = teaRouter;

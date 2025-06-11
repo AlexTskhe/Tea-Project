ef77f09a10c6a2a05649cf8bb198759d850bb2a6
AlexTskhe <tskhealexander@gmail.com>
2025-06-12 00:49:54 +0300

    user_auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router'
 // import './index.css';
 import App from './app/App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>

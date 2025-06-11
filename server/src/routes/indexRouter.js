05145b651dc2dd36059b11fb8fe91e6803866bfd
mrkqstn <myiskra@icloud.com>
2025-06-11 17:42:52 +0300

    fix

--- server/src/routes/indexRouter.js
+++ server/src/routes/indexRouter.js
@@ -7,10 +7,10 @@ const commentRouter = require('./commentRouter');
 // router.use('/auth', authRouter);
 router.use('/teas', teaRouter); //* по пути на posts отрабатывает набор из postRouter
 router.use('/comments', commentRouter);
-  
+
 //! Обработка всех запросов на несуществующие маршруты (меняем стандартный ответ от express)
 router.use((req, res) => {
-  res.status(404).json(formatResponse(404, 'Not found'));
+  res.status(404).json(formatResponse(404, 'Not found'));
 });
 
 module.exports = router;

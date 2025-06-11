05a0f4b93919e5f0603fd2b0c5df2c407ad527a1
mrkqstn <myiskra@icloud.com>
2025-06-11 14:16:09 +0300

    serverConfig, formatResponse, validId, indexRouter, cors

---
+++ server/src/routes/indexRouter.js
@@ -0,0 +1,16 @@
+const router = require('express').Router(); //* получаем экземпляр роутинга из библиотеки
+const teaRouter = require('./teaRouter'); //* подтягиваем набор роутинга по определенному пути
+const formatResponse = require('../utils/formatResponse'); //* подтягиваем утилиту для унификации ответа по 404
+const authRouter = require('./authRouter');
+const commentRouter = require('./commentRouter');
+
+router.use('/teas', teaRouter); //* по пути на posts отрабатывает набор из postRouter
+router.use('/auth', authRouter);
+router.use('/comments', commentRouter);
+  
+//! Обработка всех запросов на несуществующие маршруты (меняем стандартный ответ от express)
+router.use((req, res) => {
+  res.status(404).json(formatResponse(404, 'Not found'));
+});
+
+module.exports = router;

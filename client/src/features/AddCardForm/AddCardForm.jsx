4360eee2881bcf9772828607b4562aa8a10c8246
nick123554 <kolya21@mail.ru>
2025-06-13 16:15:06 +0300

    fix stile

--- client/src/features/AddCardForm/AddCardForm.jsx
+++ client/src/features/AddCardForm/AddCardForm.jsx
@@ -3,6 +3,8 @@ import { TeaApi } from '../../entities/teas/TeaApi';
 import { useNavigate } from 'react-router';
 import TeaValidator from '../../shared/utils/TeaValidator';
 import { UserContext } from '../../entities/User/UserContext';
+import styles from './AddCardForm.module.css';
+
 
 const defaultValue = {
   name: '',
@@ -41,24 +43,38 @@ const nav = useNavigate()
 
 
   return (
-     <>
-      <form onSubmit={addHandler}>
-        <input onChange={inputsHandler} value={inputs.name} name='name' placeholder='name' />
-        <input
-          onChange={inputsHandler}
-          value={inputs.location}
-          name='location'
-          placeholder='location'
-        />
-        <input onChange={inputsHandler} value={inputs.image} name='image' placeholder='image'/>
-        <input
-          onChange={inputsHandler}
-          value={inputs.description}
-          name='description'
-          placeholder='description'
-        />
-        <button type='submit'>Добавить</button>
-      </form>
-    </>
-  )
+  <div className={styles.formContainer}>
+    <form onSubmit={addHandler}>
+      <input
+        className={styles.input}
+        onChange={inputsHandler}
+        value={inputs.name}
+        name='name'
+        placeholder='Название чая'
+      />
+      <input
+        className={styles.input}
+        onChange={inputsHandler}
+        value={inputs.location}
+        name='location'
+        placeholder='Происхождение'
+      />
+      <input
+        className={styles.input}
+        onChange={inputsHandler}
+        value={inputs.image}
+        name='image'
+        placeholder='URL изображения'
+      />
+      <input
+        className={styles.input}
+        onChange={inputsHandler}
+        value={inputs.description}
+        name='description'
+        placeholder='Описание'
+      />
+      <button type='submit' className={styles.button}>Добавить</button>
+    </form>
+  </div>
+)
 }
